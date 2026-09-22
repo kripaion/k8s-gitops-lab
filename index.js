0f5870d22592454e758dc41e162270a99795e7ac
kripaion <kripaion.talukder@gmail.com>
2026-09-22 05:59:44 -0400

    Fix index.js to run a continuous HTTP server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("Hello GitOps");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello GitOps!\n');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
